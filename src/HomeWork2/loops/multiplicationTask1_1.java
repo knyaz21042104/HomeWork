f1a322587abe486fc906572ca2b761e525bb6d0a
M.Dziubko <knyaz21042104@yandex.ru>
2021-04-07 20:31:19 +0300

    classWork07_04

---
+++ src/HomeWork2/loops/multiplicationTask1_1.java
@@ -0,0 +1,22 @@
+package HomeWork2.loops;
+
+import java.util.Scanner;
+
+public class multiplicationTask1_1 {
+    public static void main(String[] args) {
+        System.out.print("Введите целое число :");
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        int result = 0;
+
+        for (int i = 1; i <= number; ++i){
+            if (i != number){
+                System.out.print(i + " * ");
+            }else {
+                System.out.print(result + " = ");
+            }
+            result = number * i;
+        }
+        System.out.println(result);
+    }
+}
