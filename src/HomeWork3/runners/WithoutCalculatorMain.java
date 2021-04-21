030bd2971e69fb8bc51e698089cc2f9464d4107b
knyaz21042104 <knyaz21042104@yandex.ru>
2021-04-21 18:12:17 +0300

    3/1

---
+++ src/HomeWork3/runners/WithoutCalculatorMain.java
@@ -0,0 +1,8 @@
+package HomeWork3.runners;
+
+public class WithoutCalculatorMain {
+    public static void main(String[] args) {
+        double resultat = 4.1 + 15 * 7 + (28 / 5.0) * (28 / 5.0);
+        System.out.println("4.1 + 15 * 7 + (28 / 5) ^ 2 = " + resultat);
+    }
+}
